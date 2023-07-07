2e8ab947e0b6b4b0da3f5e89bb4dd023b8d90762
yuma-matsui <oshino.iiyo@gmail.com>
2023-07-07 10:43:49 +0900

    zustandでmiddlewareの使い方を学習した

---
+++ zustand02/src/components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { FC } from "react";
+import useStore from "src/state/todos";
+
+const Header: FC = () => {
+  const todos = useStore(state => state.todos)
+  return (
+    <header>
+      <nav>
+        <h1>
+          <Link href="/">
+            <a>React状態管理</a>
+          </Link>
+        </h1>
+        <Link href="/">
+          <a>TODO一覧</a>
+        </Link>
+        <Link href="/add">
+          <a>TODO追加</a>
+        </Link>
+      </nav>
+      <h2>TODO: {todos.length}件</h2>
+    </header>
+  );
+}
+
+
+
+export default Header
---
+++ zustand02/src/pages/add.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import { ComponentProps } from "react";
+import useStore from "src/state/todos";
+
+const Add: NextPage = () => {
+  const addTodo = useStore((state) => state.addTodo)
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = (e) => {
+    e.preventDefault();
+    addTodo(e.currentTarget.text.value);
+    e.currentTarget.reset();
+  };
+
+  return (
+    <div>
+      <h3>TODO追加</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="text" autoComplete="off" required />
+        <button>追加</button>
+      </form>
+    </div>
+  );
+}
+
+export default Add;
